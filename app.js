3c58e49f002444bba57df22523ec28560550c669
Dev0071 <lecturergacheru71@gmail.com>
2022-11-07 16:10:49 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// set the initial count
+let count = 0;
+
+// select value and btn
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach( btn => {
+    btn.addEventListener('click', (e) => {
+       const styles = e.currentTarget.classList;
+
+        if(styles.contains("decrease")){
+            count --;
+        } else if (styles.contains('increase')){
+            count ++;
+        } else {
+            count = 0;
+        }
+
+
+        if(count > 0){
+            value.style.color = 'green';
+        } else if (count < 0){
+            value.style.color = 'red';
+            
+        } else{
+            value.style.color = '#222';
+        }
+
+        
+        value.textContent = count;
+    });
+});
+
